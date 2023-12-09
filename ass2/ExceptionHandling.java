ff8bb22ca753d6202849aa48840382b598ca0ba1
VinayBommagoni2904 <153353207+VinayBommagoni2904@users.noreply.github.com>
2023-12-09 13:57:54 +0530

    Add files via upload

---
+++ ass2/ExceptionHandling.java
@@ -0,0 +1,17 @@
+public class ExceptionHandling {
+public static void main(String[] args) {
+int[] numbers = {1, 2, 3, 4, 5};
+try {
+System.out.println(numbers[10]);
+int result = divide(10, 0);
+System.out.println("Result: " + result);
+} catch (ArrayIndexOutOfBoundsException e) {
+System.out.println("Array index out of bounds.");
+}
+}
+public static int divide(int a, int b) {
+return a / b;
+}
+}
+/*Exception in thread "main" java.lang.ArithmeticException: / by zero
+code which causes error should be in try block so result should be in try block*/
